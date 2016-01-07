5272eba07d7a5cf3975ce7f22b46e3d3e498905d
Harry Garrood <harry@garrood.me>
2016-01-07 00:10:08 +0000

    Remove redundant imports in example

--- example/example.purs
+++ example/example.purs
@@ -4,16 +4,13 @@ import Prelude
 
 import Data.Maybe
 import Data.Array
-import Data.Functor
 import Data.String (charAt, fromChar)
 
 import Control.Monad.Aff
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console
 
-import qualified Node.Path as Path
 import qualified Node.FS.Aff as FS
-import qualified Node.FS as FS
 import qualified Node.FS.Stats as FS
 
 main = launchAff do
