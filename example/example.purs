6b86baa835cb13d17ca54a85eb500f65e16d8b39
Phil Freeman <paf31@cantab.net>
2016-06-11 19:34:20 -0700

    Updates for 1.0 libraries (#11)
    * Updates for 1.0 libraries
    
    * CI fixes?
    
    * pulp dep -> bower
    
    * Add bower
    
    * trusty

--- example/example.purs
+++ example/example.purs
@@ -2,22 +2,27 @@ module Example.Main where
 
 import Prelude
 
-import Data.Maybe
-import Data.Array
-import Data.String (charAt, fromChar)
-
-import Control.Monad.Aff
+import Control.Monad.Aff (launchAff)
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Data.Array (filterM)
+import Data.Maybe (maybe)
+import Data.String (charAt, singleton)
+import Node.FS (FS)
+import Node.FS.Aff (stat, readdir)
+import Node.FS.Stats (isDirectory)
 
-import qualified Node.FS.Aff as FS
-import qualified Node.FS.Stats as FS
-
-main = launchAff do
-  files <- FS.readdir "."
+main :: forall eff. Eff ( err     :: EXCEPTION
+                        , fs      :: FS
+                        , console :: CONSOLE
+                        | eff
+                        ) Unit
+main = void $ launchAff do
+  files <- readdir "."
   files' <- flip filterM files \file -> do
-    stat <- FS.stat file
-    return $
-         FS.isDirectory stat
-      && (maybe false (fromChar >>> (/= ".")) $ charAt 0 file)
+    stat <- stat file
+    pure $ isDirectory stat
+      && (maybe false (singleton >>> (_ /= ".")) $ charAt 0 file)
   liftEff $ log $ show files'
