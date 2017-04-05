8982f68967464f8b0ec785a8d8e9bf699d052ec2
Phil Freeman <paf31@cantab.net>
2017-04-04 18:00:15 -0700

    Updates for 0.11

--- test/Main.purs
+++ test/Main.purs
@@ -1,4 +1,4 @@
-module Example.Main where
+module Test.Main where
 
 import Prelude
 
@@ -7,21 +7,21 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Array (filterM)
+import Data.Array (filterA)
 import Data.Maybe (maybe)
 import Data.String (charAt, singleton)
 import Node.FS (FS)
 import Node.FS.Aff (stat, readdir)
 import Node.FS.Stats (isDirectory)
 
-main :: forall eff. Eff ( err     :: EXCEPTION
-                        , fs      :: FS
-                        , console :: CONSOLE
+main :: forall eff. Eff ( exception :: EXCEPTION
+                        , fs        :: FS
+                        , console   :: CONSOLE
                         | eff
                         ) Unit
 main = void $ launchAff do
   files <- readdir "."
-  files' <- flip filterM files \file -> do
+  files' <- flip filterA files \file -> do
     stat <- stat file
     pure $ isDirectory stat
       && (maybe false (singleton >>> (_ /= ".")) $ charAt 0 file)
